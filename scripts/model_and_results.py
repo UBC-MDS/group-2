15c12eaee2369fce0b80e961706f41b0dfc16afd
Susannah Sun <thisis.susannahsun@gmail.com>
2024-12-07 00:36:28 -0800

    commented a lot of stuff we might not need; if tiffany says we dont need them, i will just delete them; minor changes to reading data in model_and_results.py

--- scripts/model_and_results.py
+++ scripts/model_and_results.py
@@ -14,17 +14,17 @@
 
 # +
 import click
-import os
-import altair as alt
-import altair_ally as aly
+# import os
+# import altair as alt
+# import altair_ally as aly
 import numpy as np
 import pandas as pd
-import pickle
+# import pickle
 # from deepchecks.tabular.checks import FeatureLabelCorrelation, FeatureFeatureCorrelation
 # from deepchecks.tabular import Dataset
 
-from ucimlrepo import fetch_ucirepo 
-from sklearn.model_selection import train_test_split
+# from ucimlrepo import fetch_ucirepo 
+# from sklearn.model_selection import train_test_split
 
 from sklearn import set_config
 from sklearn.preprocessing import StandardScaler
@@ -32,38 +32,36 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.model_selection import StratifiedKFold
+# from sklearn.model_selection import StratifiedKFold
 
 from sklearn.model_selection import RandomizedSearchCV
 import scipy.stats as stats
 
 from sklearn.metrics import accuracy_score
-from joblib import dump
+# from joblib import dump
 
 import warnings
 warnings.filterwarnings('ignore', category=FutureWarning)
-import pandera as pa
-
-from data_validation import clean_data
+# import pandera as pa
 
 @click.command()
-@click.option('--training-data', type=str, help="Path to training data")
-# @click.option('--preprocessor', type=str, help="Path to preprocessor object")
+# @click.option('--training-data', type=str, help="Path to training data")
+# @click.option('--test-data', type=str, help="Path to test data")
+# @click.option('--preprocessor_to', type=str, help="Path to preprocessor object")
 # @click.option('--columns-to-drop', type=str, help="Optional: columns to drop")
-@click.option('--pipeline-to', type=str, help="Path to directory where the pipeline object will be written to")
+# @click.option('--data-to', type=str, help="Path to directory where processed data will be written to")
+# @click.option('--pipeline-to', type=str, help="Path to directory where the pipeline object will be written to")
 # @click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
-@click.option('--seed', type=int, help="Random seed", default=522)
-def model_and_result(training_data, preprocessor, columns_to_drop, pipeline_to, plot_to, seed):
+# @click.option('--seed', type=int, help="Random seed", default=522)
+def model_and_result(training_data, test_data, preprocessor_to, columns_to_drop, pipeline_to, plot_to, data_to, seed):
     '''Fits a breast cancer classifier to the training data 
     and saves the pipeline object.'''
     np.random.seed(seed)
     set_config(transform_output="pandas")
 
-    # read in data & preprocessor
-    clean_data = ...
-    # Split training and testing data
-    train_df, test_df = train_test_split(clean_data, test_size=0.2, random_state=522)
+    # Read in training and test data
+    train_df = pd.read_csv(training_data)
+    test_df = pd.read_csv(test_data)
     # cancer_preprocessor = pickle.load(open(preprocessor, "rb"))
 
 
@@ -80,11 +78,12 @@ def model_and_result(training_data, preprocessor, columns_to_drop, pipeline_to, 
         (OneHotEncoder(drop='if_binary'), binary_features),
         (StandardScaler(), numeric_features)
     )
+    # pickle.dump(preprocessor, open(os.path.join(preprocessor_to, "wine_quality_preprocessor.pickle"), "wb"))
     
     # Make pipeline using StandardScaler and LogisticRegression
     model = make_pipeline(
         preprocessor,
-        LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+        LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=2000)
     )
 
     # Define parameter distribution
@@ -102,8 +101,13 @@ def model_and_result(training_data, preprocessor, columns_to_drop, pipeline_to, 
     # Best parameters
     print("Best Parameters:", random_search.best_params_)
 
-    with open(os.path.join(pipeline_to, "wine_quality_pipeline.pickle"), 'wb') as f:
-        pickle.dump(random_search, f)
+    # with open(os.path.join(pipeline_to, "wine_quality_pipeline.pickle"), 'wb') as f:
+    #     pickle.dump(random_search, f)
+    
+    # scaled_train = preprocessor.transform(X_train)
+    # scaled_test = preprocessor.transform(X_test)
+    # scaled_train.to_csv(os.path.join(data_to, "scaled_wine_quality_train.csv"), index=False)
+    # scaled_test.to_csv(os.path.join(data_to, "scaled_wine_quality_test.csv"), index=False)
 
     # Evaluate
     y_pred = random_search.predict(X_test)
