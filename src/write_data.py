c559014ca89e21b02107dbaabf69addd1dd87ae3
Paramveer Singh <paramveer.singh238@outlook.com>
2024-12-11 19:30:20 -0800

    Setup beginning of function and test files

---
+++ src/write_data.py
@@ -0,0 +1,11 @@
+# write_data.py
+# Author: Paramveer Singh
+# 11 December 2024
+
+# Writes Pandas DataFrames to CSV files
+
+import pandas as pd
+import os
+
+def write_data(dataframe: pd.DataFrame, path: str, filename: str) -> int:
+    pass
---
+++ tests/test_write_data.py
@@ -0,0 +1,12 @@
+# test_write_data.py
+# Author: Paramveer Singh
+# 11 December 2024
+
+# Tests write_data.py in various use cases and incorrect inputs
+
+import pytest
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.write_data import write_data
+
