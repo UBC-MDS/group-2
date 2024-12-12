e195be549f17ee9b134287cffa0ab6f3320dfee9
Paramveer Singh <paramveer.singh238@outlook.com>
2024-12-11 20:08:50 -0800

    Write success test cases

--- tests/test_write_data.py
+++ tests/test_write_data.py
@@ -13,6 +13,8 @@ from src.write_data import write_data
 
 
 # Test input data
+PATH = './test_data/'
+
 simple_dataframe = pd.DataFrame(
     {
         'x': [i for i in range(10)],
@@ -36,3 +38,37 @@ wrong_type = {'x': [0.1, 0.2],  'y': ['hit', 'miss']}
 # with 0 signifying success and other numbers signifying errors
 # The function will write a CSV and it must be tested that the output is a proper CSV
 # and matches the original DataFrame
+def test_write_data_success():
+    filename = 'simple_dataframe.csv'
+
+    status = write_data(simple_dataframe, PATH, filename)
+    
+    # Test if the returned value was int and was successful
+    assert isinstance(status, int), 'Did not return integer'
+    assert status == 0, 'CSV write has failed!'
+
+    # Test if the file was created and named correctly
+    file_path = os.path.join(PATH, filename)
+    assert os.path.isfile(file_path)
+
+    # Check if outputted CSV is the same as the test input
+    file_df = pd.read_csv(file_path)
+    pd.testing.assert_frame_equal(simple_dataframe.reset_index(drop=True), file_df)
+
+# `write_data` should also be able to handle columns of different dtypes
+def test_write_data_dtypes_success():
+    filename = 'multi_type_dataframe.csv'
+
+    status = write_data(multi_type_dataframe, PATH, filename)
+
+    # Test if the returned value was int and was succesful
+    assert isinstance(status, int), 'Did not return integer'
+    assert status == 0, 'CSV write has failed!'
+
+    # Test if the file was created and named correctly
+    file_path = os.path.join(PATH, filename)
+    assert os.path.isfile(file_path)
+
+    # Check if outputted CSV is the same as the test input
+    file_df = pd.read_csv(file_path)
+    pd.testing.assert_frame_equal(multi_type_dataframe.reset_index(drop=True), file_df)
